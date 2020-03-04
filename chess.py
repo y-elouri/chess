0742e88f74f58b7732f3584567670245ca327aee
Yassine Elouri <yassine.elouri@gmail.com>
2020-03-04 14:54:34 -0800

    wip - added legal_moves() + refactoring

--- chess.py
+++ chess.py
@@ -14,7 +14,7 @@ class PromotionError(Exception): pass
 
 class ChessBoard(NamedTuple):
     board: bytes
-    player: int
+    player: bool
     en_passant: int
     promoted: int
     w_king: int
@@ -40,39 +40,40 @@ class ChessBoard(NamedTuple):
         return r
 
     def __bool__(self) -> bool:
-        king = self.w_king if self.player == 1 else self.b_king
-        if not is_check(self.board, king):
-            return True
-        for pos in move_king(self.board, king):
-            next_board = bytearray(self.board)
-            next_board[pos], next_board[king] = next_board[king], 0
-            if not is_check(next_board, pos):
-                return True
-        return False
+        king = self.w_king if self.player else self.b_king
+        #########################
+        # get all pieces
+        # freeze legal_moves on with current board
+        # return any(map(legal_moves, pieces))
+        return True
 
-def new_game():
+    @property
+    def is_check(self):
+        return bool(self.board[self.w_king]&16) if self.player else bool(self.board[self.b_king]&16)
+
+def new_game(): #FIXME: update rook initial flags
     with open('board.bin', mode='rb') as f:
-        return ChessBoard(f.read(120), 1, 0, 0, 95, 25)
+        return ChessBoard(f.read(120), True, 0, 0, 95, 25)
 
 def promote(chess_board, choice):
     board = bytearray(chess_board.board)
     if choice == 1:
-        if chess_board.player == 1:
+        if chess_board.player:
             board[chess_board.promoted] = 6
         else:
             board[chess_board.promoted] = 38
     elif choice == 2:
-        if chess_board.player == 1:
+        if chess_board.player:
             board[chess_board.promoted] = 5
         else:
             board[chess_board.promoted] = 37
     elif choice == 3:
-        if chess_board.player == 1:
+        if chess_board.player:
             board[chess_board.promoted] = 4
         else:
             board[chess_board.promoted] = 36
     elif choice == 4:
-        if chess_board.player == 1:
+        if chess_board.player:
             board[chess_board.promoted] = 3
         else:
             board[chess_board.promoted] = 35
@@ -87,96 +88,50 @@ def promote(chess_board, choice):
         chess_board.b_king
     )
 
-def move(chess_board, square, position):
+def move(chess_board, square, position): # TODO: input validation, valid non king square + 0-0 | 0-0-0
 
-    def invalid_move(p):
+    def invalid_move(symbol):
         if position in {'0-0', '0-0-0'}:
-            raise MoveError(f'{p} {position} invalid move!')
+            raise MoveError(f'{symbol} {position} invalid move!')
         else:
-            raise MoveError(f'{p}{square} {position} invalid move!')
+            raise MoveError(f'{symbol}{square} {position} invalid move!')
 
-    s = an2i(square)
+    s = _an2i(square)
     if chess_board.board[s] == 0: # square to move from is empty
         raise SquareError(f'{square} is empty!')
-    p = i2symbol(chess_board.board[s])
+    symbol = _i2symbol(chess_board.board[s])
     try: # handle invalid input with exception for castling
-        pos = an2i(position)
+        target = _an2i(position)
         castle = False
     except SquareError:
         if chess_board.board[s]&7 == 7:
             if position == '0-0':
-                castle, pos = True, s + 2
+                castle, target = True, s + 2
             elif position == '0-0-0':
-                castle, pos = True, s - 2
+                castle, target = True, s - 2
             else:
-                invalid_move(p)
+                invalid_move(symbol)
         else:
-            invalid_move(p)
+            invalid_move(symbol)
 
     # cannot move opponent's piece or attack own pieces
-    if any([chess_board.player == 1 and chess_board.board[s]&32,
-            chess_board.player == 2 and not chess_board.board[s]&32,
-            chess_board.board[pos] and chess_board.board[s]&32 == chess_board.board[pos]&32]):
-        invalid_move(p)
+    if any([chess_board.player == chess_board.board[s]&32,
+            chess_board.board[target] and chess_board.board[s]&32 == chess_board.board[target]&32]):
+        invalid_move(symbol)
 
-    piece = chess_board.board[s]&7
-    if piece == 2:
-        moves = move_pawn(chess_board.board, s, en_passant=chess_board.en_passant)
-    elif piece == 3:
-        moves = move_knight(chess_board.board, s)
-    elif piece == 4:
-        moves = move_bishop(chess_board.board, s)
-    elif piece == 5:
-        moves = move_rook(chess_board.board, s)
-    elif piece == 6:
-        moves = move_queen(chess_board.board, s)
-    else:
-        moves = move_king(chess_board.board, s, castle=castle)
-
-    if pos not in moves: invalid_move(p)
+    if target not in legal_moves(chess_board, s, castle):
+        invalid_move(symbol)
 
     # apply move in new board
-    next_board = bytearray(chess_board.board)
-    if chess_board.player == 1:
-        king, next_king = chess_board.w_king, chess_board.b_king
+    next_board = _apply_move(chess_board.board, s, target, castle)
+    if next_board[target]&7 == 7:
+        king = target
     else:
-        king, next_king = chess_board.b_king, chess_board.w_king
-    if piece == 7 and castle:
-        next_board[pos], next_board[s] = next_board[s], 0
-        king = pos # remember king's new position
-        if pos == s+2:
-            next_board[pos-1], next_board[s+3] = next_board[s+3], 0 # castle king 0-0
-            next_board[pos-1] = 5 if chess_board.player == 1 else 37 # remove rook castling flag
-        else:
-            next_board[pos+1], next_board[s-4] = next_board[s-4], 0 # castle king 0-0-0
-            next_board[pos+1] = 5 if chess_board.player == 1 else 37 # remove rook castling flag
-    else:
-        next_board[pos], next_board[s] = next_board[s], 0
-        if piece == 7: king = pos # remember king's new position
-        if is_check(next_board, king): # king stays if move results in check
-            raise MoveError(f'{p}{square} {position} illegal move: check!')
+        king = chess_board.w_king if chess_board.player else chess_board.b_king
+    next_king = chess_board.b_king if chess_board.player else chess_board.w_king
 
-    en_passant = 0 # reset en passant    
-    promoted = pos if piece == 2 and pos // 10 in {2, 9} else 0 # promotion
-
-    if next_board[pos] == 10: ## ♙
-        next_board[pos] = 2 # remove double-step flag
-        if s - pos == 20:
-            en_passant = pos # set en passant flag
-    elif next_board[pos] == 42: ## ♟
-        next_board[pos] = 34 # remove double-step flag
-        if pos - s == 20:
-            en_passant = pos # set en passant flag
-    elif next_board[pos] == 13: ## ♖
-        next_board[pos] = 5 # remove castling flag
-    elif next_board[pos] == 45: ## ♜
-        next_board[pos] = 37 # remove castling flag
-    elif next_board[pos] == 15: ## ♔
-        next_board[pos] = 7 # remove castling flag
-    elif next_board[pos] == 47: ## ♚
-        next_board[pos] = 39 # remove castling flag
-
-    if next_board[king]&16: # remove current player's check flag
+    # remove current player's check flag
+    if next_board[king]&16:
         if next_board[king] == 31: ## ♔
             next_board[king] = 15
         if next_board[king] == 23: ## ♔
@@ -186,7 +141,35 @@ def move(chess_board, square, position):
         if next_board[king] == 55: ## ♚
             next_board[king] = 39
 
-    if is_check(next_board, next_king): # raise next player's check flag
+    # reset en passant
+    en_passant = 0
+
+    # promote pawn
+    promoted = target if next_board[target]&7 == 2 and target // 10 in {2, 9} else 0
+
+    # handle pawn special moves
+    if next_board[target] == 10: ## ♙
+        next_board[target] = 2 # remove double-step flag
+        if s - target == 20:
+            en_passant = target # set en passant flag
+    elif next_board[target] == 42: ## ♟
+        next_board[target] = 34 # remove double-step flag
+        if target - s == 20:
+            en_passant = target # set en passant flag
+    # update castling rights
+    elif next_board[target] == 5: ## ♖
+        if next_board[king] == 15:
+            next_board[king] = 7 # remove castling flag
+    elif next_board[target] == 37: ## ♜
+        if next_board[king] == 47:
+            next_board[king] = 39 # remove castling flag
+    elif next_board[target] == 15: ## ♔
+        next_board[target] = 7 # remove castling flag
+    elif next_board[target] == 47: ## ♚
+        next_board[target] = 39 # remove castling flag
+
+    # raise next player's check flag
+    if _can_attack(next_board, chess_board.player, next_king, en_passant=en_passant):
         if next_board[next_king] == 15: ## ♔
             next_board[next_king] = 31
         if next_board[next_king] == 7:  ## ♔
@@ -198,14 +181,77 @@ def move(chess_board, square, position):
 
     return ChessBoard(
         bytes(next_board),
-        2 if chess_board.player == 1 else 1,
+        not chess_board.player,
         en_passant,
         promoted,
-        king if chess_board.player == 1 else next_king,
-        next_king if chess_board.player == 1 else king
+        *((king, next_king) if chess_board.player else (next_king, king))
     )
 
-def move_pawn(board, square, en_passant=0):
+def legal_moves(chess_board, square, castle):
+    piece = chess_board.board[square]&7
+    if piece == 2:
+        moves = _move_pawn(chess_board.board, square, en_passant=chess_board.en_passant)
+    elif piece == 3:
+        moves = _move_knight(chess_board.board, square)
+    elif piece == 4:
+        moves = _move_bishop(chess_board.board, square)
+    elif piece == 5:
+        moves = _move_rook(chess_board.board, square)
+    elif piece == 6:
+        moves = _move_queen(chess_board.board, square)
+    elif piece == 7:
+        moves = _move_king(chess_board.board, square, castle=castle)
+    else:
+        pass #TODO: raise expection + refactor out switch logic including _can_attack
+    legal_moves = []
+    for move in moves:
+        next_board = _apply_move(chess_board.board, square, move, castle)
+        king = chess_board.w_king if chess_board.player else chess_board.b_king
+        if not _can_attack(next_board, not chess_board.player, king, en_passant=chess_board.en_passant):
+            legal_moves.append(move)
+    return frozenset(legal_moves)
+
+def _can_attack(board, player, target, en_passant=0):
+    if player:
+        attacks = (i for i in range(100) if not board[i]&32 and 0 < board[i] < 255)
+    else:
+        attacks = (i for i in range(100) if board[i]&32 and 0 < board[i] < 255)
+    for square in attacks:
+        piece = board[square]&7
+        if piece == 2:
+            moves = _move_pawn(board, square, en_passant=en_passant)
+        elif piece == 3:
+            moves = _move_knight(board, square)
+        elif piece == 4:
+            moves = _move_bishop(board, square)
+        elif piece == 5:
+            moves = _move_rook(board, square)
+        elif piece == 6:
+            moves = _move_queen(board, square)
+        elif piece == 7:
+            moves = _move_king(board, square)
+        else:
+            pass
+        if target in moves:
+            return True
+    return False  
+
+def _apply_move(board, square, target, castle):
+    next_board = bytearray(board)
+
+    # put piece on target square
+    next_board[target], next_board[square] = next_board[square], 0
+
+    # handle castling
+    if castle:
+        if target == square+2: # 0-0
+            next_board[target-1], next_board[square+3] = next_board[square+3], 0
+        else: # 0-0-0
+            next_board[target+1], next_board[square-4] = next_board[square-4], 0
+    
+    return next_board
+
+def _move_pawn(board, square, en_passant=0):
     moves = []
     if board[square]&32:
         if board[square+10] == 0:
@@ -231,7 +277,7 @@ def move_pawn(board, square, en_passant=0):
             moves.append(en_passant-10)
     return frozenset(moves)
 
-def move_knight(board, square):
+def _move_knight(board, square):
     moves = []
     for i in [8, 12, 19, 21]:
         if board[square+i] == 0:
@@ -244,22 +290,16 @@ def move_knight(board, square):
             moves.append(square-i)
     return frozenset(moves)
 
-def move_bishop(board, square):
-    return frozenset().union(*map(slide, [board]*4, [square]*4, [11, -11, 9, -9]))
+def _move_bishop(board, square):
+    return frozenset().union(*map(_slide, [board]*4, [square]*4, [11, -11, 9, -9]))
 
-def move_rook(board, square):
-    return frozenset().union(*map(slide, [board]*4, [square]*4, [10, -10, 1, -1]))
+def _move_rook(board, square):
+    return frozenset().union(*map(_slide, [board]*4, [square]*4, [10, -10, 1, -1]))
 
-def move_queen(board, square):
-    return move_bishop(board, square) | move_rook(board, square)
+def _move_queen(board, square):
+    return _move_bishop(board, square) | _move_rook(board, square)
 
-def move_king(board, square, castle=False):
-
-    def quick_check(board, king, pos):
-        next_board = bytearray(board)
-        next_board[pos], next_board[square] = next_board[square], 0
-        return is_check(bytes(next_board), pos)
-
+def _move_king(board, square, castle=False):
     moves = []
     for i in [1, 9, 10, 11]:
         if (board[square+i] == 0 or board[square+i] != 255 and
@@ -269,15 +309,13 @@ def move_king(board, square, castle=False):
                 board[square]&32 != board[square-i]&32):
             moves.append(square-i)
     if castle and board[square]&8 and not board[square]&16:
-        if (board[square+3]&7 == 5 and board[square+3]&8 and 0 == board[square+1] == board[square+2] and not
-                any(quick_check(board, square, pos) for pos in [square+1, square+2])):
+        if (board[square+3]&7 == 5 and 0 == board[square+1] == board[square+2]):
             moves.append(square+2)
-        if (board[square-4]&7 == 5 and board[square-4]&8 and 0 == board[square-1] == board[square-2] == board[square-3] and not
-                any(quick_check(board, square, pos) for pos in [square-1, square-2])):
+        if (board[square-4]&7 == 5 and 0 == board[square-1] == board[square-2] == board[square-3]):
             moves.append(square-2)
     return frozenset(moves)
 
-def slide(board, square, direction):
+def _slide(board, square, direction):
     moves = []
     position = square + direction
     while board[position] != 255:
@@ -290,37 +328,16 @@ def slide(board, square, direction):
         position += direction
     return frozenset(moves)
 
-def is_check(board, king):
-    if board[king]&32:
-        attacks = (i for i in range(100) if not board[i]&32 and 0 < board[i] < 255)
-    else:
-        attacks = (i for i in range(100) if board[i]&32 and 0 < board[i] < 255)
-    for square in attacks:
-        piece = board[square]&7
-        if piece == 2:
-            moves = move_pawn(board, square)
-        elif piece == 3:
-            moves = move_knight(board, square)
-        elif piece == 4:
-            moves = move_bishop(board, square)
-        elif piece == 5:
-            moves = move_rook(board, square)
-        elif piece == 6:
-            moves = move_queen(board, square)
-        else:
-            moves = move_king(board, square)
-    return True if king in moves else False  
-
-def i2symbol(i):
+def _i2symbol(i):
     return B_PIECES[(i&7) - 2] if i&32 else W_PIECES[(i&7) -2]
 
-def an2i(an):
+def _an2i(an):
     p = re.compile('^[a-h][1-8]$')
     if re.match(p, an):
         return 'abcdefgh'.index(an[0]) + 1 + (10 - int(an[1])) * 10
     raise SquareError(f'{an} is not a valid square!')
 
-def i2an(i):
+def _i2an(i):
     return '%s%d' % ('abcdefgh'[i%10 - 1], 10 - i//10)
 
 if __name__ == '__main__':
@@ -332,7 +349,7 @@ if __name__ == '__main__':
     print(chess_board)
     while True:
         try:
-            square, position, *_ = input(f"player {chess_board.player}'s turn ==> ").split()
+            square, position, *_ = input(f"player {1 if chess_board.player else 2}'s turn ==> ").split()
         except ValueError:
             print('missing input!')
             continue
@@ -355,6 +372,6 @@ if __name__ == '__main__':
         print(chess_board)
         if not chess_board:
             break
-        if is_check(chess_board.board, chess_board.w_king if chess_board.player == 1 else chess_board.b_king):
+        if chess_board.is_check:
             print('check!') 
     print(f'checkmate: player {chess_board.player} won!')
